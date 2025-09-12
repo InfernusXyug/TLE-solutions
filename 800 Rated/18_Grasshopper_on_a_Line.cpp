bca2fd5492c2ab683c7d33a920193c359c4c743e
Yug Agarwal <IMYUG2005@gmail.com>
2025-09-12 21:32:04 +0530

    Create 18_Grasshopper_on_a_Line.cpp

---
+++ 800 Rated/18_Grasshopper_on_a_Line.cpp
@@ -0,0 +1,34 @@
+#pragma GCC optimize("O3", "inline")
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+#define ll long long
+#define pb push_back
+#define endl "\n"
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
+    ordered_set;
+
+void solve () {
+    ll x, k;
+    cin >> x >> k;
+    if (x % k) cout << 1 << endl << x << endl;
+    else cout << 2 << endl << x+1 << " " << -1 << endl;
+}
+
+int main() {
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+
+    ll t;
+    cin >> t;
+    while (t--) {
+        solve ();
+    }
+    return 0;
+}
