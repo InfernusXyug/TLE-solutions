a1206fb49875dfbd47215d91f507d0ae15f1014f
Yug Agarwal <IMYUG2005@gmail.com>
2025-09-16 23:53:08 +0530

    Create 26_Serval_and_Mocha's_Array.cpp

---
+++ 800 Rated/26_Serval_and_Mocha's_Array.cpp
@@ -0,0 +1,51 @@
+#pragma GCC optimize("O3", "inline")
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+#define ll long long
+#define pb push_back
+#define endl "\n"
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
+    ordered_set;
+
+void solve () {
+    ll n;
+    cin >> n;
+    vector<ll> v(n);
+    for (ll i = 0; i < n; i++) cin >> v[i];
+    for (ll i = 0; i < n-1; i++) {
+        for (ll j = i+1; j < n; j++) {
+            if (__gcd(v[i], v[j]) < 3) {
+                cout << "Yes\n";
+                return;
+            }
+        }
+    }
+    cout << "No\n";
+}
+
+int main() {
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+    ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+    auto start = chrono::high_resolution_clock::now();
+
+    ll t;
+    cin >> t;
+    while (t--) {
+        solve ();
+    }
+
+    auto end = chrono::high_resolution_clock::now();
+    #ifndef LOCAL
+        chrono::duration<double> elapsed = end - start;
+        cerr << fixed << setprecision(10);
+        cerr << "Elapsed time: " << elapsed.count() << "s\n";
+    #endif
+
+    return 0;
+}
